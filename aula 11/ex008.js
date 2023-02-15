f9a303bc0f6b93cb5d6db2b4694ac79ffaf6d6c1
Gabriella Mansur Mesquita <gabriellamesquita314@gmail.com>
2023-02-15 11:30:05 -0300

    atualizando aulas

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou  avelocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
