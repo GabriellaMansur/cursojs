5cdf78a8f404b2b8633ed9cf9fe773ee7b9a2fa0
Gabriella Mansur Mesquita <gabriellamesquita314@gmail.com>
2023-03-06 20:22:17 -0300

    exercício

--- exercicios/ex04/script.js
+++ exercicios/ex04/script.js
@@ -1,8 +1,23 @@
 function tabuada () {
     var num = window.document.getElementById('num_tab')
     var res = window.document.getElementById('res')
-    let n = Number(num.value)
-    for(let i = 0; i <= 10; num*i){
-        res.innerHTML = `${num} X ${i} = `
+    if (num.value.length == 0 || num.value >= 1000) {
+        window.alert('Insira um valor válido')
+    } else {
+        var n = Number(num.value)
+        for(var i = 0; i <= 10; i++){
+        res.innerHTML += `${n} X ${i} = ${n*i}` + "<br/>"
     }
+    }
+
+
+
+
+
+
+
+
+
+
+    
 }
